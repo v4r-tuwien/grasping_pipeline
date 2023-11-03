19a33decaef5b5280f0df5003ed81953d7f85b6e
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2023-11-03 14:57:30 +0000

    Update launching procedure: Stuff that subscribes high bandwith data is executed on robot, other stuff on PC for computational power

--- src/pipeline_bringup.sh
+++ src/pipeline_bringup.sh
@@ -5,33 +5,39 @@ SESSION=GRASPING_PIPELINE
 tmux -2 new-session -d -s $SESSION
 tmux set -g mouse on
 
-tmux new-window -t $SESSION:0 
 tmux new-window -t $SESSION:1 
 
 ## with map setting
 tmux select-window -t $SESSION:0
 tmux split-window -h
+
 tmux select-pane -t 0
 tmux send-keys "hsrb_mode" C-m
+tmux send-keys rv
+
 tmux select-pane -t 1
 tmux send-keys "hsrb_mode" C-m
-tmux select-pane -t 0
-tmux send-keys "roslaunch hsrb_moveit_config move_group.launch"
+tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_PC.launch"
 tmux send-keys enter
-tmux select-pane -t 1
-tmux send-keys "hsrb_mode" C-m
-tmux send-keys "roslaunch grasping_pipeline statemachine.launch"
-tmux send-keys enter
+
+
 tmux select-window -t $SESSION:1
 tmux split-window -h
+
 tmux select-pane -t 0
-tmux send-keys "hsrb_mode" C-m
-tmux send-keys "roslaunch haf_grasping haf_grasping_all.launch" C-m
+tmux send-keys "ssh hsrb.local" C-m
+tmux send-keys "source ~/lexi/catkin_ws/devel/setup.bash"
+tmux send-keys enter
+tmux send-keys "roslaunch hsrb_moveit_config move_group.launch" C-m
+
 tmux select-pane -t 1
-tmux send-keys "hsrb_mode" C-m
-tmux send-keys "roslaunch table_plane_extractor get_objects_on_table_as.launch" C-m
+tmux send-keys "ssh hsrb.local" C-m
+tmux send-keys "source ~/lexi/catkin_ws/devel/setup.bash"
+tmux send-keys enter
+tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_robot.launch" C-m
+
 tmux select-window -t $SESSION:0
-tmux select-pane -t 2
+tmux select-pane -t 1
 
 tmux rename-window 'grasping'
 
