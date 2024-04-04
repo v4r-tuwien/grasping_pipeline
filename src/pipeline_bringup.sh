ec2e581116433ff61260a5c84c2ff80dfa05c6c3
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2024-04-04 17:07:08 +0000

    Split param launching into its own launch file to have less issues with find-grasp because it gets started before parameters are available

--- src/pipeline_bringup.sh
+++ src/pipeline_bringup.sh
@@ -10,18 +10,24 @@ tmux new-window -t $SESSION:1
 ## with map setting
 tmux select-window -t $SESSION:0
 tmux split-window -h
+tmux split-window -v
 
 tmux select-pane -t 0
 tmux send-keys "hsrb_mode" C-m
-tmux send-keys rv
+tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_params.launch" C-m
+tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_PC.launch"
 
 tmux select-pane -t 1
 tmux send-keys "hsrb_mode" C-m
-tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_PC.launch"
+tmux send-keys rv
 
+tmux select-pane -t 2
+tmux send-keys "ssh v4r@hsrb.local" C-m
+tmux send-keys "source ~/demos/devel/setup.bash"
+tmux send-keys enter
+tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_robot.launch" C-m
 
 tmux select-window -t $SESSION:1
-tmux split-window -h
 
 tmux select-pane -t 0
 tmux send-keys "ssh v4r@hsrb.local" C-m
@@ -29,14 +35,9 @@ tmux send-keys "source ~/demos/devel/setup.bash"
 tmux send-keys enter
 tmux send-keys "roslaunch hsrb_moveit_config move_group.launch" C-m
 
-tmux select-pane -t 1
-tmux send-keys "ssh v4r@hsrb.local" C-m
-tmux send-keys "source ~/demos/devel/setup.bash"
-tmux send-keys enter
-tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_robot.launch" C-m
 
 tmux select-window -t $SESSION:0
-tmux select-pane -t 1
+tmux select-pane -t 0
 
 tmux rename-window 'grasping'
 
