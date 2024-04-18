d3f02e8e24c8ea44f583b56b75eddb3a392c4665
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2024-04-18 15:47:28 +0000

    Fix startup script for gp in sim mode

--- src/pipeline_bringup_sim.sh
+++ src/pipeline_bringup_sim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+port=11312
 
 SESSION=GRASPING_PIPELINE
 
@@ -13,21 +14,33 @@ tmux split-window -h
 tmux split-window -v
 
 tmux select-pane -t 0
-tmux send-keys "sim_mode" C-m
+tmux send-keys "export ROS_MASTER_URI=http://$ROS_IP:$port" C-m
 tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_params.launch && roslaunch grasping_pipeline grasping_pipeline_PC.launch"
 
 tmux select-pane -t 1
-tmux send-keys "sim_mode" C-m
+tmux send-keys "export ROS_MASTER_URI=http://$ROS_IP:$port" C-m
 tmux send-keys rv
 
 tmux select-pane -t 2
+tmux send-keys "export ROS_MASTER_URI=http://$ROS_IP:$port" C-m
 tmux send-keys "roslaunch grasping_pipeline grasping_pipeline_robot.launch"
 
 tmux select-window -t $SESSION:1
+tmux split-window -h
+tmux split-window -v
 
 tmux select-pane -t 0
-tmux send-keys "roslaunch hsrb_moveit_config move_group.launch" C-m
+tmux send-keys "export ROS_MASTER_URI=http://$ROS_IP:$port" C-m
+tmux send-keys "roslaunch hsrb_moveit_config move_group.launch"
+#tmux send-keys "roslaunch hsrb_moveit_config hsrb_demo_with_controller.launch" C-m
 
+tmux select-pane -t 1
+tmux send-keys "export ROS_MASTER_URI=http://$ROS_IP:$port" C-m
+tmux send-keys "roscore -p $port" C-m
+
+tmux select-pane -t 2
+tmux send-keys "export ROS_MASTER_URI=http://$ROS_IP:$port" C-m
+tmux send-keys "roslaunch hsrb_gazebo_launch hsrb_mock_home_world.launch"
 
 tmux select-window -t $SESSION:0
 tmux select-pane -t 0
